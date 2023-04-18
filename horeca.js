e1180b92da18625b069946dc7dd4693e790b7f54
AZIZ <azeznsasser2004@GMAIL.COM>
2023-04-18 09:56:45 +0200

    horeca

--- horeca.js
+++ horeca.js
@@ -1,6 +1,52 @@
-let drankjes = ["bier","wijn","fris"]
-let prijzen = ["bier = 3","wijn = 2","fris = 4"]
-let vraag = prompt("wat wilt u bestelen?")
+const fris = 2.00;
+const bier = 2.50;
+const wijn = 3.00;
+var bonnetje = [];
 
-
-document.getElementById("horecaa").innerText += lijstje + '\n';
+while (true) {
+    let product = prompt("Welk product wil je toevoegen? (of typ 'stop' om te stoppen)");
+    if (product == "stop") {
+        break;
+    } else {
+        if (product !== "fris" && product !== "bier" && product !== "wijn") {
+            alert("Dit ken ik niet.");
+        } else {
+            let aantal = prompt("Hoeveel " + product + " wil je hebben?");
+            let gevonden = false;
+            for (var i = 0; i < bonnetje.length; i++) {
+                if (bonnetje[i].product == product) {
+                    bonnetje[i].aantal = parseInt(bonnetje[i].aantal) + parseInt(aantal);
+                    gevonden = true;
+                    break;
+                }
+            }
+            if (!gevonden) {
+                bonnetje.push({ product: product, aantal: aantal });
+            }
+            alert(aantal + " " + product + " is toegevoegd aan het bonnetje.");
+        }
+    }
+}
+if (bonnetje.length > 0) {
+    var bonnetjeOutput = "Bonnetje:<br>";
+    var totaalprijs = 0;
+    for (var i = 0; i < bonnetje.length; i++) {
+        var product = bonnetje[i].product;
+        var aantal = bonnetje[i].aantal;
+        var prijs = 0;
+        if (product === "fris") {
+            prijs = fris;
+        }
+        else if (product === "bier") {
+            prijs = bier;
+        } 
+        else if (product === "wijn") {
+            prijs = wijn;
+        }
+        var productTotaal = prijs * aantal;
+        bonnetjeOutput += aantal + " " + product + " " + prijs.toFixed(2)  + " " + productTotaal.toFixed(2) + " ";
+        totaalprijs += productTotaal;
+    }
+    bonnetjeOutput += "Totaal: " + " " + totaalprijs.toFixed(2) + " " + " euro";
+    document.getElementById("output").innerHTML = bonnetjeOutput;
+}
